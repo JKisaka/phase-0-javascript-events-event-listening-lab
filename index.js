f51958a6cade72ed65da75a2e1a428b613e3fa42
JOEKISAKA <joekisaka007@gmail.com>
2023-10-09 14:35:42 +0300

    Test not passing

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+function addingEventListener(button) {
+    const input = document.getElementById("input");
+    if("button"){
+        element.addingEventListener("click", function(){
+            alert("I was clicked!");
+        })
+    }
+
+    input.addEventListener("click", clickAlert);
 }
+
+// document.addEventListener("DOMContentLoaded", function"{
+// const input = document.getElementById("input");
+
+// input.AddEventListener("click", function(){
+//  alert("I was clicked!");    
+// });
+// return true;
+// })
